d2116e7982480c7f5220e93ee719f2bfa4799183
Osman Dandia <odandia@gmail.com>
2014-04-07 15:10:25 -0700

    Update to Campsite API based version

---
+++ js/utility.js
@@ -0,0 +1,66 @@
+var monthNames = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
+
+function getDate(timestamp) {
+    var date_obj = new Date(timestamp);
+    var date_str = date_obj.getUTCDate() < 10 ? "0" + date_obj.getUTCDate() : date_obj.getUTCDate().toString();
+    
+    return monthNames[date_obj.getUTCMonth()]+' '+date_str;
+}
+
+/**
+ * Takes starts_at and ends_at timestamps
+ * Returns a pretty date range string
+ */
+function getDateRangeString(starts_at, ends_at) {
+    var start = new Date(starts_at);
+    var end   = new Date(ends_at);
+
+    var startDate = monthNames[start.getUTCMonth()]+' '+start.getUTCDate();
+    var startYear = start.getUTCFullYear();
+
+    var endDate = monthNames[end.getUTCMonth()]+' '+end.getUTCDate();
+    var endYear  = end.getUTCFullYear();
+
+    if (startYear == endYear) {
+        return (startDate == endDate) ? startDate+', '+startYear : startDate+' - '+endDate+', '+startYear;
+    } else {
+        return startDate+', '+startYear+' - '+endDate+', '+endYear;
+    }
+}
+
+/**
+ * Takes starts_at and ends_at timestamps
+ * Returns a pretty time range string
+ */
+function getTimeRangeString(starts_at, ends_at) {
+
+    var start       = new Date(starts_at);
+    var end         = new Date(ends_at);
+
+    var startHours  = start.getUTCHours();
+    var endHours    = end.getUTCHours();
+    var startSuffix = 'am';
+    var endSuffix   = 'am';
+
+    if (startHours > 12) {
+        startHours -= 12;
+        startSuffix = 'pm';
+    }
+    if (endHours > 12) {
+        endHours -= 12;
+        endSuffix = 'pm';
+    }
+
+    var startMinutes = start.getUTCMinutes();
+    var endMinutes   = end.getUTCMinutes();
+
+    if (startMinutes < 10) {
+        startMinutes = '0' + startMinutes;
+    }
+    if (endMinutes < 10) {
+        endMinutes = '0' + endMinutes;
+    }
+
+    return startHours+':'+startMinutes+startSuffix+' - '+endHours+':'+endMinutes+endSuffix;
+}
+ 
