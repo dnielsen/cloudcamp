11c555f77c7f44ab4f0466e728fecfe12526a13a
gimballock <gimballock@gmail.com>
2014-08-19 19:37:25 -0700

    modularized the utility.js file

--- js/utility.js
+++ js/utility.js
@@ -1,73 +1,82 @@
-var monthNames = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
+"use strict";
 
-function getParameterByName(name) {
-    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-        results = regex.exec(location.search);
-    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
-}
+//Create utility module to give utility functions a namespace
+var UTIL = (function(){
+    var my = {};
 
-function getDate(timestamp) {
-    var date_obj = new Date(timestamp);
-    var date_str = date_obj.getUTCDate() < 10 ? "0" + date_obj.getUTCDate() : date_obj.getUTCDate().toString();
-    
-    return monthNames[date_obj.getUTCMonth()]+' '+date_str;
-}
+    // private attributes or functions are just local to this scope
+    // public attributes or functions are assigned to the return value 'my'
 
-/**
- * Takes starts_at and ends_at timestamps
- * Returns a pretty date range string
- */
-function getDateRangeString(starts_at, ends_at) {
-    var start = new Date(starts_at);
-    var end   = new Date(ends_at);
+    my.monthNames = [ "Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec" ];
 
-    var startDate = monthNames[start.getUTCMonth()]+' '+start.getUTCDate();
-    var startYear = start.getUTCFullYear();
+    my.getParameterByName = function (name) {
+        name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+            results = regex.exec(location.search);
+        return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    };
 
-    var endDate = monthNames[end.getUTCMonth()]+' '+end.getUTCDate();
-    var endYear  = end.getUTCFullYear();
+    my.getDate = function (timestamp) {
+        var date_obj = new Date(timestamp);
+        var date_str = date_obj.getUTCDate() < 10 ? "0" + date_obj.getUTCDate() : date_obj.getUTCDate().toString();
+        return my.monthNames[date_obj.getUTCMonth()]+' '+date_str;
+    };
 
-    if (startYear == endYear) {
-        return (startDate == endDate) ? startDate+', '+startYear : startDate+' - '+endDate+', '+startYear;
-    } else {
-        return startDate+', '+startYear+' - '+endDate+', '+endYear;
-    }
-}
+    /**
+     * Takes starts_at and ends_at timestamps
+     * Returns a pretty date range string
+     */
+    my.getDateRangeString = function (starts_at, ends_at) {
+        var start = new Date(starts_at);
+        var end   = new Date(ends_at);
 
-/**
- * Takes starts_at and ends_at timestamps
- * Returns a pretty time range string
- */
-function getTimeRangeString(starts_at, ends_at) {
+        var startDate = monthNames[start.getUTCMonth()]+' '+start.getUTCDate();
+        var startYear = start.getUTCFullYear();
 
-    var start       = new Date(starts_at);
-    var end         = new Date(ends_at);
+        var endDate = monthNames[end.getUTCMonth()]+' '+end.getUTCDate();
+        var endYear  = end.getUTCFullYear();
 
-    var startHours  = start.getUTCHours();
-    var endHours    = end.getUTCHours();
-    var startSuffix = 'am';
-    var endSuffix   = 'am';
+        if (startYear == endYear) {
+            return (startDate == endDate) ? startDate+', '+startYear : startDate+' - '+endDate+', '+startYear;
+        } else {
+            return startDate+', '+startYear+' - '+endDate+', '+endYear;
+        }
+    };
 
-    if (startHours > 12) {
-        startHours -= 12;
-        startSuffix = 'pm';
-    }
-    if (endHours > 12) {
-        endHours -= 12;
-        endSuffix = 'pm';
-    }
+    /**
+     * Takes starts_at and ends_at timestamps
+     * Returns a pretty time range string
+     */
+    my.getTimeRangeString = function (starts_at, ends_at) {
+        var start       = new Date(starts_at);
+        var end         = new Date(ends_at);
 
-    var startMinutes = start.getUTCMinutes();
-    var endMinutes   = end.getUTCMinutes();
+        var startHours  = start.getUTCHours();
+        var endHours    = end.getUTCHours();
+        var startSuffix = 'am';
+        var endSuffix   = 'am';
 
-    if (startMinutes < 10) {
-        startMinutes = '0' + startMinutes;
-    }
-    if (endMinutes < 10) {
-        endMinutes = '0' + endMinutes;
-    }
+        if (startHours > 12) {
+            startHours -= 12;
+            startSuffix = 'pm';
+        }
+        if (endHours > 12) {
+            endHours -= 12;
+            endSuffix = 'pm';
+        }
 
-    return startHours+':'+startMinutes+startSuffix+' - '+endHours+':'+endMinutes+endSuffix;
-}
- 
+        var startMinutes = start.getUTCMinutes();
+        var endMinutes   = end.getUTCMinutes();
+
+        if (startMinutes < 10) {
+            startMinutes = '0' + startMinutes;
+        }
+        if (endMinutes < 10) {
+            endMinutes = '0' + endMinutes;
+        }
+
+        return startHours+':'+startMinutes+startSuffix+' - '+endHours+':'+endMinutes+endSuffix;
+    };
+
+    return my;
+}());
